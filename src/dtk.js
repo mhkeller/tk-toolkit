e90854cb717873a4dd8c5d00c44a05681bb8c9ec
mhkeller <michael.keller@gmail.com>
2014-02-16 14:37:58 -0500

    add write data, reorganize

--- src/dtk.js
+++ src/dtk.js
@@ -3,16 +3,19 @@ var fs          = require('fs'),
     toDbf       = require('dbf'),
     fromDbf     = require('node-dbf');
 
-function discernFormat(file_name){
-  var name_arr = file_name.split('\.')
-  format_name = name_arr[name_arr.length - 1];
-  return format_name
-}
-
-function discernParser(file_name, delimiter){
-  if (delimiter) return dsv(delimiter)
-  var format = discernFormat(file_name);
-  return parsers[format]
+var formatters = {
+  json: function(file){
+    return JSON.stringify(file)
+  },
+  csv: function(file){
+    return dsv.csv.format(file)
+  },
+  tsv: function(file){
+    return dsv.tsv.format(file)
+  },
+  psv: function(file){
+    return dsv.dsv('|').format(file)
+  }
 }
 
 var parsers = {
@@ -23,59 +26,96 @@ var parsers = {
   Dbf_Parser: require('node-dbf')
 }
 
-var readers = {
-  readCsv: function(path, cb){
-    fs.readFile(path, 'utf8', function(err, data){
-      cb(err, parsers.csv.parse(data));    
-    })
-  },
-  readCsvSync: function(path){
-    return parsers.csv.parse(fs.readFileSync(path, 'utf8'));
-  },
-  readJson: function(path, cb){
-    fs.readFile(path, function(err, data){
-      cb(err, JSON.parse(data));
-    })
-  },
-  readJsonSync: function(path){
-    return parsers.JSON.parse(fs.readFileSync(path));
-  },
-  readTsv: function(path, cb){
-    fs.readFile(path, 'utf8', function(err, data){
-      cb(err, parsers.tsv.parse(data));    
-    })
-  },
-  readTsvSync: function(path){
-    return parsers.tsv.parse(fs.readFileSync(path, 'utf8'));
-  },
-  readPsv: function(path, cb){
-    fs.readFile(path, 'utf8', function(err, data){
-      cb(err, parsers.psv.parse(data));    
-    })
-  },
-  readPsvSync: function(path){
-    return parsers.psv.parse(fs.readFileSync(path, 'utf8'));
-  },
-  readData: function(path, cb, delimiter){
-    fs.readFile(path, 'utf8', function(err, data){
-      cb(err, discernParser(path, delimiter).parse(data));    
-    })
-  },
-  readDataSync: function(path, delimiter){
-    return discernParser(path, delimiter).parse(fs.readFileSync(path, 'utf8'));
-  },
-  readDbf: function(path){
-    var dbf_parser = new parsers.Dbf_Parser(path),
-        rows = [];
-    dbf_parser.on('record', function(p) {
-        rows.push(p)
-    });
-    dbf_parser.on('end', function(p) {
-        // console.log(rows)
-    });
-    dbf_parser.parse();
+var helpers = {};
 
-  }
+// Given a file name, determine its extension
+// Used by: discernParser, discernFileFormatter
+helpers.discernFormat = function(file_name) {
+  var name_arr = file_name.split('\.')
+  format_name  = name_arr[name_arr.length - 1];
+  return format_name
+}
+
+// Given a file name, optionally a delimiter, return a parser that will turn that file into json
+helpers.discernParser = function(file_name, delimiter){
+  if (delimiter) return dsv(delimiter)
+  var format = discernFormat(file_name);
+  return parsers[format]
+}
+
+// Given a file name, return a parser that will convert that json object to its extension
+helpers.discernFileFormatter = function(file_name){
+  var format   = discernFormat(file_name);
+  return formatters[format]
+}
+
+var readers = {}
+readers.readCsv: function(path, cb){
+  fs.readFile(path, 'utf8', function(err, data){
+    cb(err, parsers.csv.parse(data));    
+  })
+}
+readers.readCsvSync: function(path){
+  return parsers.csv.parse(fs.readFileSync(path, 'utf8'));
+}
+readers.readJson: function(path, cb){
+  fs.readFile(path, function(err, data){
+    cb(err, JSON.parse(data));
+  })
+}
+readers.readJsonSync: function(path){
+  return parsers.JSON.parse(fs.readFileSync(path));
+}
+readers.readTsv: function(path, cb){
+  fs.readFile(path, 'utf8', function(err, data){
+    cb(err, parsers.tsv.parse(data));    
+  })
+}
+readers.readTsvSync: function(path){
+  return parsers.tsv.parse(fs.readFileSync(path, 'utf8'));
+}
+readers.readPsv: function(path, cb){
+  fs.readFile(path, 'utf8', function(err, data){
+    cb(err, parsers.psv.parse(data));    
+  })
+}
+readers.readPsvSync: function(path){
+  return parsers.psv.parse(fs.readFileSync(path, 'utf8'));
+}
+readers.readData: function(path, cb, delimiter){
+  fs.readFile(path, 'utf8', function(err, data){
+    cb(err, discernParser(path, delimiter).parse(data));    
+  })
+}
+readers.readDataSync: function(path, delimiter){
+  return discernParser(path, delimiter).parse(fs.readFileSync(path, 'utf8'));
+}
+// readDbf: function(path){
+  //   var dbf_parser = new parsers.Dbf_Parser(path),
+  //       rows = [];
+  //   dbf_parser.on('record', function(p) {
+  //       rows.push(p)
+  //   });
+  //   dbf_parser.on('end', function(p) {
+  //       // console.log(rows)
+  //   });
+  //   dbf_parser.parse();
+
+  // }
+//}
+
+var writers = {};
+
+// Given a file path, some json data, convert it to that format and write it
+writers.writeData = function(path, data, cb){
+  var fileFormatter = discernFileFormatter(file_path);
+  fs.WriteFile(file_path, fileFormatter(data), function(err){
+    cb(err)
+  })
+}
+writers.writeDataSync = function(path, data){
+  var fileFormatter = discernFileFormatter(file_path);
+  fs.WriteFileSync(file_path, fileFormatter(data))
 }
 
 module.exports = {
@@ -89,5 +129,6 @@ module.exports = {
   readPsvSync: readers.readPsvSync,
   readData: readers.readData,
   readDataSync: readers.readDataSync,
-  readDbf: readers.readDbf
+  writeData: writers.writeData,
+  writeDataSync: writers.writeDataSync,
 }
